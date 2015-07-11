3638645e124b031c0ae93095e06f2ca206c37ca1
Robert Lee-Cann <robert@spicerack.co.uk>
2015-07-11 13:45:02 +0100

    Use 'clearance' gem to handle authentication.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -17,6 +17,7 @@ RSpec.configure do |config|
   config.infer_base_class_for_anonymous_controllers = false
   config.infer_spec_type_from_file_location!
   config.use_transactional_fixtures = false
+  config.include Features::ClearanceHelpers, type: :feature
 end
 
 ActiveRecord::Migration.maintain_test_schema!
