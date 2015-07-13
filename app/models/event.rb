62adb136d382a4beec5e1bdb4d65d6a5586e6fd4
Robert Lee-Cann <robert@spicerack.co.uk>
2015-07-13 13:11:11 +0100

    Allow events to be activated/deactivated

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,12 @@ class Event < ActiveRecord::Base
   belongs_to :user
 
   validates :name, presence: true
+
+  def activate!
+    update(active: true)
+  end
+
+  def deactivate!
+    update(active: false)
+  end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -3,4 +3,22 @@ require "rails_helper"
 describe Event do
   it { is_expected.to validate_presence_of :name }
   it { is_expected.to belong_to :user }
+
+  describe "#activate!" do
+    it "activate an event" do
+      event = create(:event)
+      event.activate!
+
+      expect(event.active).to be_truthy
+    end
+  end
+
+  describe "#deactivate!" do
+    it "deactivates an event" do
+      event = create(:event, active: true)
+      event.deactivate!
+
+      expect(event.active).to be_falsey
+    end
+  end
 end
